2c6cc1d4c1813c8d19fced79e271b8a531b23be0
Lucrecia <lucre252@gmail.com>
2024-11-09 00:29:16 -0300

    Funcionamiento de perfil

--- JS/sidebar.js
+++ JS/sidebar.js
@@ -127,3 +127,23 @@ window.onclick = function(event) {
         }
     }
 }
+
+//Profile
+
+function toggleDropdown() {
+    var dropdown = document.getElementById("userDropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
+document.addEventListener('click', function(event) {
+    var isClickInside = document.querySelector('.user-menu').contains(event.target);
+    var dropdown = document.getElementById("userDropdown");
+
+    if (!isClickInside) {
+        dropdown.style.display = "none";
+    }
+});
